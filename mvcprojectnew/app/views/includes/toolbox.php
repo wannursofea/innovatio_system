17afb720768fecd63dff7784b2b9ab3f83d1935d
lowyingxi <lowxi@graduate.utm.my>
2024-01-01 20:59:30 +0800

    extract toolbox from begin_app

---
+++ mvcprojectnew/app/views/includes/toolbox.php
@@ -0,0 +1,43 @@
+	<!--begin::Toolbar-->
+							<div id="kt_app_toolbar" class="app-toolbar w-100">
+								<!--begin::Toolbar container-->
+								<div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-column">
+									<!--begin::Wrapper-->
+									<div class="d-flex flex-stack my-10 my-lg-14">
+										<!--begin::Page title-->
+										<div class="page-title d-flex flex-column justify-content-center me-3">
+											<!--begin::Title-->
+											<h1 class="page-heading d-flex title-custom fw-bolder fs-2hx flex-column justify-content-center my-0">Online Courses</h1>
+											<!--end::Title-->
+											<!--begin::Breadcrumb-->
+											<ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+												<!--begin::Item-->
+												<li class="breadcrumb-item text-gray-700">
+													<a href="index.html" class="text-gray-700 text-hover-primary">Home</a>
+												</li>
+												<!--end::Item-->
+												<!--begin::Item-->
+												<li class="breadcrumb-item">
+													<span class="">
+														<i class="ki-duotone ki-right fs-6 text-gray-700"></i>
+													</span>
+												</li>
+												<!--end::Item-->
+												<!--begin::Item-->
+												<li class="breadcrumb-item text-gray-700">Dash</li>
+												<!--end::Item-->
+											</ul>
+											<!--end::Breadcrumb-->
+										</div>
+										<!--end::Page title-->
+
+
+
+                                        <?php require APPROOT . '/views/includes/action.php';?>
+
+									</div>
+									<!--end::Wrapper-->
+								</div>
+								<!--end::Toolbar container-->
+							</div>
+							<!--end::Toolbar-->
