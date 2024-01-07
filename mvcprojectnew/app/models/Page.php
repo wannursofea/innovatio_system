1af4b91adffe40ec0775512bbe3ee7c15dfec9c5
MUHAMMAD DANIAL BIN AHMAD SYAHIR <danial03@graduate.utm.my>
2024-01-07 23:53:38 +0800

    Update Page.php

--- mvcprojectnew/app/models/Page.php
+++ mvcprojectnew/app/models/Page.php
@@ -12,7 +12,7 @@ class Page
     public function studentProfile()
     {
 
-        $this->db->query("SELECT * FROM st_profile WHERE st_email = :email");
+        $this->db->query("SELECT * FROM st_profile WHERE email = :email");
 
         $this->db->bind(':email', $_SESSION['email']);
 
@@ -65,34 +65,35 @@ class Page
     {
 
  
-        if (isset($data['st_image'])) {
+        if (isset($data['image'])) {
 
         $this->db->query("UPDATE st_profile 
-        SET st_email = :email, st_ic = :st_ic, st_fullname = :st_fullname, st_gender = :st_gender,
-        st_race = :st_race, univ_code  = :univ_code, st_address  = :st_address, st_image  = :st_image WHERE st_email   = :email;");
+        SET email = :email, phoneNum = :phoneNum, name = :name, gender = :gender,
+        race = :race, institution  = :institution, address  = :address, image  = :image WHERE email   = :email;");
 
         $this->db->bind(':email', $_SESSION['email']);
-        $this->db->bind(':st_ic', $data['st_ic']);
-        $this->db->bind(':st_fullname', $data['st_fullname']);
-        $this->db->bind(':st_gender', $data['st_gender']);
-        $this->db->bind(':st_race', $data['st_race']);
-        $this->db->bind(':st_address', $data['st_address']);
-        $this->db->bind(':univ_code', $data['univ_code']);
-        $this->db->bind(':st_image', $data['st_image']);
+        $this->db->bind(':phoneNum', $data['phoneNum']);
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':gender', $data['gender']);
+        $this->db->bind(':race', $data['race']);
+        $this->db->bind(':address', $data['address']);
+        $this->db->bind(':institution', $data['institution']);
+        $this->db->bind(':image', $data['image']);
 
         }else{
 
         $this->db->query("UPDATE st_profile 
-        SET st_email = :email, st_ic = :st_ic, st_fullname = :st_fullname, st_gender = :st_gender,
-        st_race = :st_race, univ_code  = :univ_code, st_address  = :st_address WHERE st_email = :email;");
+        SET email = :email, phoneNum = :phoneNum, name = :name, gender = :gender,
+        race = :race, institution  = :institution, address  = :address, image  = :image WHERE email   = :email;");
 
         $this->db->bind(':email', $_SESSION['email']);
-        $this->db->bind(':st_ic', $data['st_ic']);
-        $this->db->bind(':st_fullname', $data['st_fullname']);
-        $this->db->bind(':st_gender', $data['st_gender']);
-        $this->db->bind(':st_race', $data['st_race']);
-        $this->db->bind(':st_address', $data['st_address']);
-        $this->db->bind(':univ_code', $data['univ_code']);
+        $this->db->bind(':phoneNum', $data['phoneNum']);
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':gender', $data['gender']);
+        $this->db->bind(':race', $data['race']);
+        $this->db->bind(':address', $data['address']);
+        $this->db->bind(':institution', $data['institution']);
+        $this->db->bind(':image', $data['image']);
             
         }
         
