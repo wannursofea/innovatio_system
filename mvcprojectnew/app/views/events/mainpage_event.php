24bce806383e27815cd93479fcc7702503a7964e
lowyingxi <lowxi@graduate.utm.my>
2024-01-09 20:27:34 +0800

    Update mainpage_event.php

--- mvcprojectnew/app/views/events/mainpage_event.php
+++ mvcprojectnew/app/views/events/mainpage_event.php
@@ -10,12 +10,274 @@
 												<!--begin::Separator-->
 												<div class="separator separator-dashed mb-9"></div>
 												<!--end::Separator-->
+
+
+
+
+												<!--begin::Row-->
+												<div class="row g-5 gx-xl-10 mb-5 mb-xl-10">
+													<!--begin::Col-->
+													<div class="col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10">
+														<!--begin::Card widget 20-->
+														<div class="card card-flush bgi-no-repeat bgi-size-contain bgi-position-x-end h-md-50 mb-5 mb-xl-10" style="background-color: #F1416C;background-image:url('assets/media/patterns/vector-1.png')">
+															<!--begin::Header-->
+															<div class="card-header pt-5">
+																<!--begin::Title-->
+																<div class="card-title d-flex flex-column">
+																	<!--begin::Amount-->
+																	<span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">69</span>
+																	<!--end::Amount-->
+																	<!--begin::Subtitle-->
+																	<span class="text-white opacity-75 pt-1 fw-semibold fs-6">Total number of event</span>
+																	<!--end::Subtitle-->
+																</div>
+																<!--end::Title-->
+															</div>
+															<!--end::Header-->
+															<!--begin::Card body-->
+															<div class="card-body d-flex align-items-end pt-0">
+																
+															</div>
+															<!--end::Card body-->
+														</div>
+														<!--end::Card widget 20-->
+														
+													</div>
+													<!--end::Col-->
+													<!--begin::Col-->
+													<div class="col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10">
+														<!--begin::Card widget 17-->
+														<div class="card card-flush h-md-50 mb-5 mb-xl-10">
+															<!--begin::Header-->
+															<div class="card-header pt-5">
+																<!--begin::Title-->
+																<div class="card-title d-flex flex-column">
+																	<!--begin::Info-->
+																	<div class="d-flex align-items-center">
+																		<!--begin::Currency-->
+																		<span class="fs-4 fw-semibold text-gray-500 me-1 align-self-start">$</span>
+																		<!--end::Currency-->
+																		<!--begin::Amount-->
+																		<span class="fs-2hx fw-bold text-gray-900 me-2 lh-1 ls-n2">69,700</span>
+																		<!--end::Amount-->
+																		<!--begin::Badge-->
+																		<span class="badge badge-light-success fs-base">
+																		<i class="ki-duotone ki-arrow-up fs-5 text-success ms-n1">
+																			<span class="path1"></span>
+																			<span class="path2"></span>
+																		</i>2.2%</span>
+																		<!--end::Badge-->
+																	</div>
+																	<!--end::Info-->
+																	<!--begin::Subtitle-->
+																	<span class="text-gray-500 pt-1 fw-semibold fs-6">Projects Earnings in April</span>
+																	<!--end::Subtitle-->
+																</div>
+																<!--end::Title-->
+															</div>
+															<!--end::Header-->
+															<!--begin::Card body-->
+															<div class="card-body pt-2 pb-4 d-flex flex-wrap align-items-center">
+																
+															</div>
+															<!--end::Card body-->
+														</div>
+														<!--end::Card widget 17-->
+														<!--begin::List widget 26-->
+														<div class="card card-flush h-lg-50">
+															<!--begin::Header-->
+															<div class="card-header pt-5">
+																<!--begin::Title-->
+																<h3 class="card-title text-gray-800 fw-bold">External Links</h3>
+																<!--end::Title-->
+																<!--begin::Toolbar-->
+																<div class="card-toolbar">
+																	<!--begin::Menu-->
+																	<button class="btn btn-icon btn-color-gray-500 btn-active-color-primary justify-content-end" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-overflow="true">
+																		<i class="ki-duotone ki-dots-square fs-1 text-gray-500 me-n1">
+																			<span class="path1"></span>
+																			<span class="path2"></span>
+																			<span class="path3"></span>
+																			<span class="path4"></span>
+																		</i>
+																	</button>
+																	<!--begin::Menu 2-->
+																	<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px" data-kt-menu="true">
+																		<!--begin::Menu item-->
+																		<div class="menu-item px-3">
+																			<div class="menu-content fs-6 text-gray-900 fw-bold px-3 py-4">Quick Actions</div>
+																		</div>
+																		<!--end::Menu item-->
+																		<!--begin::Menu separator-->
+																		<div class="separator mb-3 opacity-75"></div>
+																		<!--end::Menu separator-->
+																		<!--begin::Menu item-->
+																		<div class="menu-item px-3">
+																			<a href="#" class="menu-link px-3">New Ticket</a>
+																		</div>
+																		<!--end::Menu item-->
+																		<!--begin::Menu item-->
+																		<div class="menu-item px-3">
+																			<a href="#" class="menu-link px-3">New Customer</a>
+																		</div>
+																		<!--end::Menu item-->
+																		<!--begin::Menu item-->
+																		<div class="menu-item px-3" data-kt-menu-trigger="hover" data-kt-menu-placement="right-start">
+																			<!--begin::Menu item-->
+																			<a href="#" class="menu-link px-3">
+																				<span class="menu-title">New Group</span>
+																				<span class="menu-arrow"></span>
+																			</a>
+																			<!--end::Menu item-->
+																			<!--begin::Menu sub-->
+																			<div class="menu-sub menu-sub-dropdown w-175px py-4">
+																				<!--begin::Menu item-->
+																				<div class="menu-item px-3">
+																					<a href="#" class="menu-link px-3">Admin Group</a>
+																				</div>
+																				<!--end::Menu item-->
+																				<!--begin::Menu item-->
+																				<div class="menu-item px-3">
+																					<a href="#" class="menu-link px-3">Staff Group</a>
+																				</div>
+																				<!--end::Menu item-->
+																				<!--begin::Menu item-->
+																				<div class="menu-item px-3">
+																					<a href="#" class="menu-link px-3">Member Group</a>
+																				</div>
+																				<!--end::Menu item-->
+																			</div>
+																			<!--end::Menu sub-->
+																		</div>
+																		<!--end::Menu item-->
+																		<!--begin::Menu item-->
+																		<div class="menu-item px-3">
+																			<a href="#" class="menu-link px-3">New Contact</a>
+																		</div>
+																		<!--end::Menu item-->
+																		<!--begin::Menu separator-->
+																		<div class="separator mt-3 opacity-75"></div>
+																		<!--end::Menu separator-->
+																		<!--begin::Menu item-->
+																		<div class="menu-item px-3">
+																			<div class="menu-content px-3 py-3">
+																				<a class="btn btn-primary btn-sm px-4" href="#">Generate Reports</a>
+																			</div>
+																		</div>
+																		<!--end::Menu item-->
+																	</div>
+																	<!--end::Menu 2-->
+																	<!--end::Menu-->
+																</div>
+																<!--end::Toolbar-->
+															</div>
+															<!--end::Header-->
+															<!--begin::Body-->
+															<div class="card-body pt-5">
+																<!--begin::Item-->
+																<div class="d-flex flex-stack">
+																	<!--begin::Section-->
+																	<a href="#" class="text-primary fw-semibold fs-6 me-2">Avg. Client Rating</a>
+																	<!--end::Section-->
+																	<!--begin::Action-->
+																	<button type="button" class="btn btn-icon btn-sm h-auto btn-color-gray-500 btn-active-color-primary justify-content-end">
+																		<i class="ki-duotone ki-exit-right-corner fs-2">
+																			<span class="path1"></span>
+																			<span class="path2"></span>
+																		</i>
+																	</button>
+																	<!--end::Action-->
+																</div>
+																<!--end::Item-->
+																<!--begin::Separator-->
+																<div class="separator separator-dashed my-3"></div>
+																<!--end::Separator-->
+																<!--begin::Item-->
+																<div class="d-flex flex-stack">
+																	<!--begin::Section-->
+																	<a href="#" class="text-primary fw-semibold fs-6 me-2">Instagram Followers</a>
+																	<!--end::Section-->
+																	<!--begin::Action-->
+																	<button type="button" class="btn btn-icon btn-sm h-auto btn-color-gray-500 btn-active-color-primary justify-content-end">
+																		<i class="ki-duotone ki-exit-right-corner fs-2">
+																			<span class="path1"></span>
+																			<span class="path2"></span>
+																		</i>
+																	</button>
+																	<!--end::Action-->
+																</div>
+																<!--end::Item-->
+																<!--begin::Separator-->
+																<div class="separator separator-dashed my-3"></div>
+																<!--end::Separator-->
+																<!--begin::Item-->
+																<div class="d-flex flex-stack">
+																	<!--begin::Section-->
+																	<a href="#" class="text-primary fw-semibold fs-6 me-2">Google Ads CPC</a>
+																	<!--end::Section-->
+																	<!--begin::Action-->
+																	<button type="button" class="btn btn-icon btn-sm h-auto btn-color-gray-500 btn-active-color-primary justify-content-end">
+																		<i class="ki-duotone ki-exit-right-corner fs-2">
+																			<span class="path1"></span>
+																			<span class="path2"></span>
+																		</i>
+																	</button>
+																	<!--end::Action-->
+																</div>
+																<!--end::Item-->
+															</div>
+															<!--end::Body-->
+														</div>
+														<!--end::LIst widget 26-->
+													</div>
+													<!--end::Col-->
+													<!--begin::Col-->
+													<div class="col-xxl-6">
+														<!--begin::Engage widget 10-->
+														<div class="card card-flush h-md-100">
+															<!--begin::Body-->
+															<div class="card-body d-flex flex-column justify-content-between mt-9 bgi-no-repeat bgi-size-cover bgi-position-x-center pb-0" style="background-position: 100% 50%; background-image:url('assets/media/stock/900x600/42.png')">
+																<!--begin::Wrapper-->
+																<div class="mb-10">
+																	<!--begin::Title-->
+																	<div class="fs-2hx fw-bold text-gray-800 text-center mb-13">
+																	<span class="me-2">Try our all new Enviroment with 
+																	<br />
+																	<span class="position-relative d-inline-block text-danger">
+																		<a href="pages/user-profile/overview.html" class="text-danger opacity-75-hover">Pro Plan</a>
+																		<!--begin::Separator-->
+																		<span class="position-absolute opacity-15 bottom-0 start-0 border-4 border-danger border-bottom w-100"></span>
+																		<!--end::Separator-->
+																	</span></span>for Free</div>
+																	<!--end::Title-->
+																	<!--begin::Action-->
+																	<div class="text-center">
+																		<a href='#' class="btn btn-sm btn-dark fw-bold" data-bs-toggle="modal" data-bs-target="#kt_modal_upgrade_plan">Upgrade Now</a>
+																	</div>
+																	<!--begin::Action-->
+																</div>
+																<!--begin::Wrapper-->
+																<!--begin::Illustration-->
+																<img class="mx-auto h-150px h-lg-200px theme-light-show" src="assets/media/illustrations/misc/upgrade.svg" alt="" />
+																<img class="mx-auto h-150px h-lg-200px theme-dark-show" src="assets/media/illustrations/misc/upgrade-dark.svg" alt="" />
+																<!--end::Illustration-->
+															</div>
+															<!--end::Body-->
+														</div>
+														<!--end::Engage widget 10-->
+													</div>
+													<!--end::Col-->
+												</div>
+												<!--end::Row-->
 												<!--begin::Row-->
 												<div class="row">
 													<!--begin::Col-->
 													<div class="col-md-6">
 														<!--begin::Feature post-->
 														<div class="h-100 d-flex flex-column justify-content-between pe-lg-6 mb-lg-0 mb-10">
+
+															
+
 															<!--begin::Video-->
 															<div class="mb-3">
 																<iframe class="embed-responsive-item card-rounded h-275px w-100" src="https://www.youtube.com/embed/TWdDZYNqlg4" allowfullscreen="allowfullscreen"></iframe>
