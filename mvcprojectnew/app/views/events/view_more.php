924b264dd21f0a4a2ecb324bc98d8c90b0225792
lowyingxi <lowxi@graduate.utm.my>
2024-01-09 20:32:02 +0800

    Create view_more.php

---
+++ mvcprojectnew/app/views/events/view_more.php
@@ -0,0 +1,123 @@
+									<!--begin::details View-->
+									<div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+										<!--begin::Card header-->
+										<div class="card-header cursor-pointer">
+											<!--begin::Card title-->
+											<div class="card-title m-0">
+												<h3 class="fw-bold m-0">Participant Details</h3>
+											</div>
+											<!--end::Card title-->
+										</div>
+										<!--begin::Card header-->
+										<!--begin::Card body-->
+										<div class="card-body p-9">
+											<!--begin::Row-->
+											<div class="row mb-7">
+												<!--begin::Label-->
+												<label class="col-lg-4 fw-semibold text-muted">Full Name</label>
+												<!--end::Label-->
+												<!--begin::Col-->
+												<div class="col-lg-8">
+													<span class="fw-bold fs-6 text-gray-800">Max Smith</span>
+												</div>
+												<!--end::Col-->
+											</div>
+											<!--end::Row-->
+											<!--begin::Input group-->
+											<div class="row mb-7">
+												<!--begin::Label-->
+												<label class="col-lg-4 fw-semibold text-muted">Company</label>
+												<!--end::Label-->
+												<!--begin::Col-->
+												<div class="col-lg-8 fv-row">
+													<span class="fw-semibold text-gray-800 fs-6">Keenthemes</span>
+												</div>
+												<!--end::Col-->
+											</div>
+											<!--end::Input group-->
+											<!--begin::Input group-->
+											<div class="row mb-7">
+												<!--begin::Label-->
+												<label class="col-lg-4 fw-semibold text-muted">Contact Number</label>
+												<!--end::Label-->
+												<!--begin::Col-->
+												<div class="col-lg-8 d-flex align-items-center">
+													<span class="fw-bold fs-6 text-gray-800 me-2">044 3276 454 935</span>
+												</div>
+												<!--end::Col-->
+											</div>
+											<!--end::Input group-->
+											<!--begin::Input group-->
+											<div class="row mb-7">
+												<!--begin::Label-->
+												<label class="col-lg-4 fw-semibold text-muted">Email</label>
+												<!--end::Label-->
+												<!--begin::Col-->
+												<div class="col-lg-8">
+													<a href="#" class="fw-semibold fs-6 text-gray-800 text-hover-primary">keenthemes.com</a>
+												</div>
+												<!--end::Col-->
+											</div>
+											<!--end::Input group-->
+											<!--begin::Input group-->
+											<div class="row mb-7">
+												<!--begin::Label-->
+												<label class="col-lg-4 fw-semibold text-muted">Date of Birth</label>
+												<!--end::Label-->
+												<!--begin::Col-->
+												<div class="col-lg-8">
+													<span class="fw-bold fs-6 text-gray-800">Germany</span>
+												</div>
+												<!--end::Col-->
+											</div>
+											<!--end::Input group-->
+											<!--begin::Input group-->
+											<div class="row mb-7">
+												<!--begin::Label-->
+												<label class="col-lg-4 fw-semibold text-muted">Communication</label>
+												<!--end::Label-->
+												<!--begin::Col-->
+												<div class="col-lg-8">
+													<span class="fw-bold fs-6 text-gray-800">Email, Phone</span>
+												</div>
+												<!--end::Col-->
+											</div>
+											<!--end::Input group-->
+											<!--begin::Input group-->
+											<div class="row mb-10">
+												<!--begin::Label-->
+												<label class="col-lg-4 fw-semibold text-muted">Allow Changes</label>
+												<!--begin::Label-->
+												<!--begin::Label-->
+												<div class="col-lg-8">
+													<span class="fw-semibold fs-6 text-gray-800">Yes</span>
+												</div>
+												<!--begin::Label-->
+											</div>
+											<!--end::Input group-->
+											<!--begin::Notice-->
+											<div class="notice d-flex bg-light-warning rounded border-warning border border-dashed p-6">
+												<!--begin::Icon-->
+												<i class="ki-duotone ki-information fs-2tx text-warning me-4">
+													<span class="path1"></span>
+													<span class="path2"></span>
+													<span class="path3"></span>
+												</i>
+												<!--end::Icon-->
+												<!--begin::Wrapper-->
+												<div class="d-flex flex-stack flex-grow-1">
+													<!--begin::Content-->
+													<div class="fw-semibold">
+														<h4 class="text-gray-900 fw-bold">We need your attention!</h4>
+														<div class="fs-6 text-gray-700">Your payment was declined. To start using tools, please 
+														<a class="fw-bold" href="account/billing.html">Add Payment Method</a>.</div>
+													</div>
+													<!--end::Content-->
+												</div>
+												<!--end::Wrapper-->
+											</div>
+											<!--end::Notice-->
+										</div>
+										<!--end::Card body-->
+									</div>
+									<!--end::details View-->
