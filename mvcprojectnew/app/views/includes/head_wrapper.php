4df56b3849c08f2b522ca0f646f409d2e0fd4e69
MUHAMMAD DANIAL BIN AHMAD SYAHIR <danial03@graduate.utm.my>
2024-01-14 20:38:54 +0800

    Update head_wrapper.php

--- mvcprojectnew/app/views/includes/head_wrapper.php
+++ mvcprojectnew/app/views/includes/head_wrapper.php
@@ -869,8 +869,8 @@
 								<!--begin::User menu-->
 								<div class="app-navbar-item" id="kt_header_user_menu_toggle">
 									<!--begin::Menu wrapper-->
-									<div class="cursor-pointer symbol symbol-35px symbol-lg-50px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-										<img src="assets/media/avatars/300-13.jpg" alt="user" />
+									<div class="cursor-pointer symbol symbol-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+										<img src="<?php echo URLROOT . "/public/" . $_SESSION['user_image']; ?> "class="rounded-3" alt="user" />
 									</div>
 									<!--begin::User account menu-->
 									<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -879,8 +879,8 @@
 											<div class="menu-content d-flex align-items-center px-3">
 												<!--begin::Avatar-->
 												<div class="symbol symbol-50px me-5">
-													<img alt="Logo" src="assets/media/avatars/300-13.jpg" />
-												</div>
+													<img alt="Logo" src="<?php echo URLROOT . "/public/" . $_SESSION['user_image']; ?>" />
+												</div>	
 												<!--end::Avatar-->
 												<!--begin::Username-->
 												<div class="d-flex flex-column">
