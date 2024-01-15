dcc890a35adb2e5a9ca875249c1b8c8498ed92b3
lowyingxi <lowxi@graduate.utm.my>
2024-01-16 04:53:09 +0800

    Update register.php

--- mvcprojectnew/app/views/users/register.php
+++ mvcprojectnew/app/views/users/register.php
@@ -14,7 +14,7 @@
 		<meta property="og:url" content="https://keenthemes.com/metronic" />
 		<meta property="og:site_name" content="Metronic by Keenthemes" />
 		<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-		<link rel="shortcut icon" href="<?php echo URLROOT ?>/public/assets/media/logos/favicon.ico" />
+		<link rel="shortcut icon" href="<?php echo URLROOT ?>/public/assets/media/YVlogo.jpeg" />
 		<!--begin::Fonts(mandatory for all pages)-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
 		<!--end::Fonts-->
