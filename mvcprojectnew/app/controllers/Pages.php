18131f09dbd31afaaa2421fc5f015d480e57a641
Wan Nur Sofea <wannursofea@graduate.utm.my>
2024-01-15 16:35:02 +0800

    update profile client

--- mvcprojectnew/app/controllers/Pages.php
+++ mvcprojectnew/app/controllers/Pages.php
@@ -10,11 +10,62 @@ class Pages extends Controller {
 
 
     public function index() {
+        $student='';
+        $partner='';
+        if (!isLoggedIn()) {
+            header("Location: " . URLROOT. "/pages" );
+            $user_id = $_SESSION['user_id'];      
+            
+            $student = $this->pageModel->findStudentById($user_id); //To get data from student table
+            $partner = $this->pageModel->findPartnerById($user_id);
+         
+        }
+    
         $data = [
-            'title' => 'Home page'
+            'title' => 'Home page',
+            'student' => $student,
+            'partner' => $partner,
+        ];
+        $this->view('index', $data);
+    }
+
+    public function dashboard(){
+        $user_id = $_SESSION['user_id'];      
+        
+        $student = $this->pageModel->findStudentById($user_id); //To get data from student table
+        $profile_id = '';   
+        //retrieve the data that already have in the system - get student
+        if ($student) {
+            $profile_id = $student->profile_id;
+        }
+        if(!$user_id){
+            die("Profile is not existing.");
+        }
+
+
+        $studentInfo = $this->pageModel->studentInfo($user_id); // retrieve student table data
+        $badgeInfo = $this->pageModel->badgeInfo($profile_id); // retrieve student table data
+        $eventInfo = $this->pageModel->eventInfo();
+
+        $eventNum = 0;
+
+        foreach($eventInfo as $event){
+            if($event->event_id){
+                $eventNum =+ 1;
+            }
+        }
+
+
+        $data = 
+        [
+            'student' => $studentInfo,
+            'badge' => $badgeInfo,
+            'event' => $eventInfo,
+            'eventNum' => $eventNum,
         ];
 
         $this->view('index', $data);
+
     }
 
     public function edit_profile()
@@ -127,7 +178,6 @@ class Pages extends Controller {
 
             }
 
-            //var_dump($data);
 
           if ($_POST['update_student']) {
                 if ($this->pageModel->updateStudentProfile($data)) {
@@ -148,4 +198,127 @@ class Pages extends Controller {
 
         $this->view('pages/index', $data);
     }
+
+    public function edit_client()
+    {
+       
+
+        //check for post from form
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') { //if server request open
+
+            //sanitize post data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            
+
+            // Check if file was uploaded without errors
+            if (isset($_FILES["file"]) && $_FILES["file"]["error"] == 0) {
+                $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+                $filename = $_FILES["file"]["name"];
+                $filetype = $_FILES["file"]["type"];
+                $filesize = $_FILES["file"]["size"];
+
+                $fileExt = explode('.', $filename);
+                $fileActualExt = strtolower(end($fileExt));
+
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
+                if (!array_key_exists($ext, $allowed)){
+                    $_SESSION['failed'] = "Error: You cannot upload files of this type!";
+                    header("Location: " . URLROOT . "/pages/edit_client");
+                }
+
+                $username = $_SESSION['username'];
+                $maxsize = 5 * 1024 * 1024;
+                if ($filesize > $maxsize){
+                    $_SESSION['failed'] = "Error: File size is larger than the allowed limit.";
+                            header("Location: " . URLROOT . "/pages/edit_client");
+                } 
+                $location = "images/users/" . $username;
+
+                if (in_array($filetype, $allowed)) {
+
+                    if (file_exists($location . $filename)) {
+                        echo $filename . " is already exists.";
+                    } else {
+                        
+                            # create directory if not exists in upload/ directory
+                            if (!is_dir($location)) {
+                                //mkdir($location, 0755);
+                                mkdir('images/users/' . $username, 0777, true);
+                            }
+
+                            $fileNameNew = uniqid('', true) . "." . $fileActualExt;
+
+                            $location .= "/" . $fileNameNew;
+
+                            move_uploaded_file($_FILES['file']['tmp_name'], $location);
+                    }
+                } else {
+                    $_SESSION['failed'] = "Error: There was an error uploading your file!";
+                        header("Location: " . URLROOT . "/pages/edit_client");
+                }
+            } else {
+
+                $_SESSION['failed'] = "Error: There was an error uploading your file!";
+                        header("Location: " . URLROOT . "/pages/edit_client");
+              
+            }
+
+            // $_POST['update_student'] hidden value from form
+           if ($_POST['update_client']) {
+
+                if (isset($_FILES["file"]) && $_FILES["file"]["error"] == 0) {
+
+                    $data = [
+
+
+    
+                        'email' => trim($_POST['email']),
+                        'companyName' => trim($_POST['companyName']),
+                        'companyDescription' => trim($_POST['companyDescription']),
+                        'city' => trim($_POST['city']),
+                        'country' => trim($_POST['country']),
+                        'officeNum' => trim($_POST['officeNum']),
+                        'pr_image' => $location
+    
+                    ];
+
+                }else{
+
+                    $data = [
+
+                        'email' => trim($_POST['email']),
+                        'companyName' => trim($_POST['companyName']),
+                        'companyDescription' => trim($_POST['companyDescription']),
+                        'city' => trim($_POST['city']),
+                        'country' => trim($_POST['country']),
+                        'officeNum' => trim($_POST['officeNum']),
+                        
+               
+                    ];
+                }
+
+            }
+
+
+          if ($_POST['update_client']) {
+                if ($this->pageModel->updateClientProfile($data)) {
+                    header("Location: " . URLROOT . "/pages/edit_client");
+                } else {
+                    die("Something went wrong, please try again!");
+                }
+            } else {
+                $this->view('pages/index');
+            }
+        } // end of if statement 
+
+        $clientProfile = $this->pageModel->clientProfile();
+        
+        $data = [
+            'clientProfile' => $clientProfile
+        ];
+
+        $this->view('pages/index', $data);
+    }
+    
 }
---
+++ mvcprojectnew/app/views/pages/edit_client.php
@@ -0,0 +1,100 @@
+<div class="card shadow-sm">
+    <div class="card-header">
+        <h3 class="card-title">Profile Details</h3>
+        <div class="card-toolbar">
+      
+        </div>
+    </div>
+    <div class="card-body">
+                
+                <?php foreach ($data['clientProfile'] as $clientProfile) :?>
+                <?php endforeach; ?>
+                
+                                  
+<form action="<?php echo URLROOT; ?>/pages/edit_client" method="POST" class="form" enctype="multipart/form-data" id="kt_account_profile_details_form">
+    <div class="card-body border-top p-9">
+        <!-- Avatar Section -->
+        <div class="row mb-6">
+            <label class="col-lg-4 col-form-label fw-semibold fs-6">Avatar</label>
+            <div class="col-lg-8">
+                <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url('<?php echo URLROOT ."/public/".$clientProfile->pr_image; ?>')">
+                    <div class="image-input-wrapper w-125px h-125px" style="background-image: url('<?php echo URLROOT."/public/".$clientProfile->pr_image; ?>')"></div>
+                    <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
+                        <i class="ki-duotone ki-pencil fs-7"></i>
+                        <input type="file" name="file" accept=".png, .jpg, .jpeg" />
+                        <input type="hidden" name="profile_avatar_remove" />
+                    </label>
+                    <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Cancel avatar">
+                        <i class="ki-duotone ki-cross fs-2"></i>
+                    </span>
+                    <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Remove avatar">
+                        <i class="ki-duotone ki-cross fs-2"></i>
+                    </span>
+                </div>
+                <div class="form-text">Allowed file types: png, jpg, jpeg.</div>
+            </div>
+        </div>
+
+        <!-- Full Name Section -->
+        <div class="row mb-6">
+            <label class="col-lg-4 col-form-label required fw-semibold fs-6">Company Name</label>
+            <div class="col-lg-8">
+                <input class="form-control form-control-lg form-control-solid" name="companyName" type="text" required value="<?php echo $clientProfile->companyName; ?>" />
+            </div>
+        </div>
+
+        <!-- Phone Number Section -->
+        <div class="row mb-6">
+            <label class="col-lg-4 col-form-label required fw-semibold fs-6">Office Number (Example:012-3456789)</label>
+            <div class="col-lg-8">
+                <input class="form-control form-control-lg form-control-solid" name="officeNum" type="text" required value="<?php echo $clientProfile->officeNum; ?>" />
+            </div>
+        </div>
+        
+ 
+        <!-- Email Address Section -->
+        <div class="row mb-6">
+            <label class="col-lg-4 col-form-label required fw-semibold fs-6">Email Address</label>
+            <div class="col-lg-8">
+                <input class="form-control form-control-lg form-control-solid" name="email" type="text" readonly value="<?php echo $clientProfile->email; ?>" />
+            </div>
+        </div>
+
+
+        <!-- City Section -->
+        <div class="row mb-6">
+            <label class="col-lg-4 col-form-label required fw-semibold fs-6">City</label>
+            <div class="col-lg-8">
+                <textarea class="form-control form-control-solid" name="city" rows="3" required value><?php echo $clientProfile->city ?></textarea>
+            </div>
+        </div>
+
+        <!-- Country Section -->
+        <div class="row mb-6">
+            <label class="col-lg-4 col-form-label required fw-semibold fs-6">Country</label>
+            <div class="col-lg-8">
+                <textarea class="form-control form-control-solid" name="country" rows="3" required value><?php echo $clientProfile->country ?></textarea>
+            </div>
+        </div>
+
+        <!-- Bio Section -->
+        <div class="row mb-6">
+            <label class="col-lg-4 col-form-label fw-semibold fs-6">Company Description</label>
+            <div class="col-lg-8">
+                <textarea class="form-control form-control-solid" name="companyDescription" rows="3"><?php echo $clientProfile->companyDescription ?></textarea>
+            </div>
+
+        <!-- Submit Button -->
+        <div class="card-footer d-flex justify-content-end py-6 px-9">
+            <input type="hidden" id="update_client" name="update_client" value="update_client">
+            <button type="submit" name="submit" class="btn btn-primary">Update</button>
+        </div>
+    </div>
+</form>
+
+
+    </div>
+    <div class="card-footer">
+        Youth Venture
+    </div>
+</div>
